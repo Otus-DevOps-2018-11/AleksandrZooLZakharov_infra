8fc1a06c7558fe27c7d3ce524351771e3c57f065
Aleksandr Zakharov <zoolgle@gmail.com>
2019-03-12 17:46:52 +0300

    6th homework with 1 star

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable project {
+  description = "Project ID"
+}
+
+variable user {
+  description = "Ssh username"
+}
+
+variable user1 {
+  description = "Ssh username"
+}
+
+variable user2 {
+  description = "Ssh username"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+variable user1_public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable user2_public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable zone {
+  description = "Zone in Region"
+  default     = "europe-west1-d"
+}

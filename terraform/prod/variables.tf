6d9d022913407f2c22b4cae174ddcf3c68bfbef3
Aleksandr Zakharov <zoolgle@gmail.com>
2019-03-14 10:46:46 +0300

    7th wh base

---
+++ terraform/prod/variables.tf
@@ -0,0 +1,55 @@
+variable project {
+  description = "Project ID"
+}
+
+variable user {
+  description = "Ssh username"
+}
+
+variable user1 {
+  description = "Ssh username"
+}
+
+variable user2 {
+  description = "Ssh username"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable user_public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable user_private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+
+variable user1_public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable user2_public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable zone {
+  description = "Zone in Region"
+  default     = "europe-west1-d"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
